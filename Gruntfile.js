be23871ee758cf9aa5a6dd18c1808aaf3e3818aa
Marina Boeva <camado6pota@yandex.ru>
2016-10-03 17:53:34 +0300

    added some script

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+          dist: {
+            src: ['src/*.js'],
+            dest: 'js/script.min.js',
+          }
+        },
+        concat_css:{
+          all: {
+            src: ["src/*.css"],
+            dest: "css/style.min.css"
+          },
+        },
+        uglify: {
+          build: {
+            src: 'src/*.js',
+            dest: 'js/script.min.js'
+          }
+        },
+        cssmin: {
+          target: {
+            files: [{
+              src: ["css/style.min.css"],
+              dest: "css/style.min.css"
+            }]
+          }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-concat-css');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['concat','concat_css','uglify','cssmin']);
+
+};
